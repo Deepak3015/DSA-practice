9477361d65a2b15ff01117f5631900c004adf07c
Deepak3015 <d03748651@gmail.com>
2024-09-06 12:36:11 +0530

    DSA day(2)

--- DSA1.cpp
+++ DSA1.cpp
@@ -1,19 +1,81 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-     int a;
-     int b;
-     cout << "Enter Your First Number :"<<endl;
-     cin >> a;
-     cout <<  "Enter Your Second Number :"<<endl;
-     cin >> b;
+// int main (){
+//      int a;
+//      cout<<"Enter Your Number Here :";
+//      cin>>a;
+//      int total = 1;
+//      for (int i = 1;i<=a;i++){
+//           total = i*total;
+//      }
+//      cout<<"The Factorial oF ("<<a<<") is "<<total<<endl;
+// }
+
+
+
+
+// int main (){
+//      int a;
+//      cout << "Enter your Number:";
+//      cin >> a;
+//      int total =0;
+//      for (int i = 1;i<a;i++){
+//           if (i%3 == 0){
+//                total+=i;
+
+//           }
+
+//      }
+//      cout << total;
+//      return 0 ;
+// }
+
+
+     // int a;
+     // int b;
+     // cout << "Enter Your First Number :"<<endl;
+     // cin >> a;
+     // cout <<  "Enter Your Second Number :"<<endl;
+     // cin >> b;
      
-     cout << "The Sum Of The Given Numbers:"<<(a+b)<<endl;
+     // cout << "The Sum Of The Given Numbers:"<<(a+b)<<endl;
 
 
 
 
-     return 0;
+     // return 0;
+     // int a;
 
-}
+     // cout << "Enter your Number :"<<endl;
+     // cin >> a;
+     // if (a % 2 == 0){
+     //      cout << "The Number is Even "<<endl;
+
+     // }
+     // else {
+     //      cout << "The Number is odd"<< endl;
+     // }
+
+// int main () {
+//      int a;
+//      cout << "Enter YOur Number :"<<endl;
+//      cin>> a;
+//     bool isPrime = true;
+//       for (int i = 2;i<a;i++){
+//           if (a%i == 0){
+//                isPrime = false;
+//                break; }}
+//       if (isPrime && a>1){
+//           cout << a<<"->is an Prime Number"<<endl;
+//      }
+//      else{
+//          cout << a <<"->is not an Prime Number"<<endl;
+//      }
+
+//                return 0;
+// }
+
+
+
+
