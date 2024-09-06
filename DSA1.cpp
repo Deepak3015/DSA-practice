c8400c8791dd22b9da06ac7631800499c12a59af
Deepak3015 <d03748651@gmail.com>
2024-09-06 09:31:39 +0530

    Initial

--- DSA1.cpp
+++ DSA1.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 using namespace std;
 
- int main(){
-  int a =2;
-  int b = 4;
-  cout <<"Sum :" << (a+b) <<endl;
-  cout <<"difference :" << (a-b) <<endl; 
-  return 0;
- }
+int main(){
+     int a;
+     int b;
+     cout << "Enter Your First Number :"<<endl;
+     cin >> a;
+     cout <<  "Enter Your Second Number :"<<endl;
+     cin >> b;
+     
+     cout << "The Sum Of The Given Numbers:"<<(a+b)<<endl;
+
+
+
+
+     return 0;
+
+}
