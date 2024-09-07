8b6654a8b1ef1b280ac5ade2e932e084e4061771
Deepak3015 <d03748651@gmail.com>
2024-09-07 20:00:59 +0530

    DSA day(3)

--- DSA1.cpp
+++ DSA1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int main (){
+     int n = 5;
+     for(int i= 1;i<=5;i++){
+          cout<<i<<endl;
+          
+     }
+     
+}
+
+
+
 // int main (){
 //      int a;
 //      cout<<"Enter Your Number Here :";
